8a839a2bd9511b81ee3dd623f31c9e9494739fb3
Martin Connolly <mdconn_uk@hotmail.com>
2015-04-23 23:00:41 +0100

    Tidying up
    Tidying up functions and comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,21 +1,19 @@
-## These 2 functions are used to create a matrix and to return and
-## cache the inverse of the matrix, then if we want to return the
-## the inverse of the matrix in future it will return the cached
-## version instead of calculating it again
+## These 2 functions are used to create a matrix and to return and cache the
+## inverse of the matrix, then if we want to return the inverse of the matrix in
+## future it will return the cached version instead of calculating it again.
 
-## This function (makeCacheMatrix) takes a matrix and first checks
-## that this is a square matrix it then creates the following 4
-## functions
+## This function (makeCacheMatrix) takes a matrix and first checks that this is
+## a square matrix it then creates the following 4 functions
 ## 1. sets the matrix
 ## 2. gets the matrix
 ## 3. sets the inverse of the matrix
 ## 4. gets the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-        num_dim <- length(dim(x)) #find number of dimensions of matrix
-        if(dim(x)[1] != dim(x)[2]) {
+        if(dim(x)[1] != dim(x)[2]) {      #check if this isn't a square matrix
                 stop('Not a square matrix! Dimensions (',
                      dim(x)[1], ' x ', dim(x)[2], ' )')
+                # Error message if this isn't a square matrix
         }
         inverted <- NULL
         set <- function(y) {
@@ -32,11 +30,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## This function (cacheSolve) checks to see if we have already
-## calculated the inverse of the cached matrix we'd created earlier
-## with the makeCacheMatrix function, if so it returns the already
-## calculated inverse matrix, otherwise it sets the inverted variable,
-## which is the inverse of the matrix and returns it.
+## This function (cacheSolve) checks to see if we have already calculated the
+## inverse of the cached matrix we'd created earlier with the makeCacheMatrix
+## function, if so it returns the already calculated inverse matrix, otherwise
+## it sets the inverted object to the inverse of the matrix and returns it.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
